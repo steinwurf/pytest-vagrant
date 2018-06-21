7afc06f9cf1cb46eef3df2fdc77dc8157d610026
Jeppe Pihl <jpihl08@gmail.com>
2018-06-21 10:34:42 +0200

    added missing test

---
+++ src/pytest_vagrant/utils.py
@@ -0,0 +1,24 @@
+import os
+
+def walk_up(bottom):
+    """
+    mimic os.walk, but walk 'up'
+    instead of down the directory tree
+    """
+    while True:
+        bottom = os.path.realpath(bottom)
+        dirs, nondirs = [], []
+        for name in os.listdir(bottom):
+            if os.path.isdir(os.path.join(bottom, name)):
+                dirs.append(name)
+            else:
+                nondirs.append(name)
+
+        yield bottom, dirs, nondirs
+
+        new_path = os.path.realpath(os.path.join(bottom, '..'))
+
+        # see if we are at the top
+        if new_path == bottom:
+            return
+        bottom = new_path
