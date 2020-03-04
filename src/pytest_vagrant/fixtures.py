0f6bc7a5dee3371b4053a86e14864f17c8a87985
Morten V. Pedersen <morten@mortenvp.com>
2020-03-04 15:14:41 +0100

    Working on it

--- src/pytest_vagrant/fixtures.py
+++ src/pytest_vagrant/fixtures.py
@@ -5,8 +5,28 @@ from __future__ import absolute_import, print_function
 import pytest
 
 import pytest_vagrant.vagrant
-from pytest_vagrant.sshdirectory import SSHDirectory
 
+
+# @pytest.fixture(scope='session')
+# def vagrant(request):
+#     """ Creates the py.test fixture to make it usable withing the unit
+#     tests. See the Vagrant class for more information.
+#     """
+
+#     vagrantfile = request.config.getoption('vagrantfile')
+
+#     vagrant = pytest_vagrant.vagrant.Vagrant(vagrantfile=vagrantfile)
+
+#     if request.config.getoption('vagrantreset'):
+#         snapshots = vagrant.snapshot_list()
+
+#         if 'vagrantreset' in snapshots:
+#             vagrant.snapshot_restore(snapshot='vagrantreset')
+#         else:
+#             vagrant.reset()
+#             vagrant.snapshot_save(snapshot='vagrantreset')
+
+#     return vagrant
 
 @pytest.fixture(scope='session')
 def vagrant(request):
@@ -14,20 +34,7 @@ def vagrant(request):
     tests. See the Vagrant class for more information.
     """
 
-    vagrantfile = request.config.getoption('vagrantfile')
-
-    vagrant = pytest_vagrant.vagrant.Vagrant(vagrantfile=vagrantfile)
-
-    if request.config.getoption('vagrantreset'):
-        snapshots = vagrant.snapshot_list()
-
-        if 'vagrantreset' in snapshots:
-            vagrant.snapshot_restore(snapshot='vagrantreset')
-        else:
-            vagrant.reset()
-            vagrant.snapshot_save(snapshot='vagrantreset')
-
-    return vagrant
+    return pytest_vagrant.vagrant.Vagrant()
 
 
 def pytest_addoption(parser):
@@ -40,38 +47,38 @@ def pytest_addoption(parser):
         help='We want to reset the machine with every use')
 
 
-@pytest.fixture(scope='session')
-def sshdirectory_pytest(vagrant):
-    """ Create a pytest directory for all the ssh directories to be
-    created in.
-    """
+# @pytest.fixture(scope='session')
+# def sshdirectory_pytest(vagrant):
+#     """ Create a pytest directory for all the ssh directories to be
+#     created in.
+#     """
 
-    with vagrant.ssh() as ssh:
+#     with vagrant.ssh() as ssh:
 
-        # Get the home dir
-        _, stdout, _ = ssh.client.exec_command('cd ~;pwd')
-        if stdout.channel.recv_exit_status() != 0:
-            raise RuntimeError("Could not determine home dir")
+#         # Get the home dir
+#         _, stdout, _ = ssh.client.exec_command('cd ~;pwd')
+#         if stdout.channel.recv_exit_status() != 0:
+#             raise RuntimeError("Could not determine home dir")
 
-        cwd = stdout.read().strip()
+#         cwd = stdout.read().strip()
 
-        # We create our test directory in the home dir
-        home_dir = SSHDirectory(ssh=ssh.client, sftp=ssh.sftp, cwd=cwd)
+#         # We create our test directory in the home dir
+#         home_dir = SSHDirectory(ssh=ssh.client, sftp=ssh.sftp, cwd=cwd)
 
-        if home_dir.contains_dir('pytest_temp'):
-            home_dir.rmdir('pytest_temp')
+#         if home_dir.contains_dir('pytest_temp'):
+#             home_dir.rmdir('pytest_temp')
 
-        pytest_dir = home_dir.mkdir('pytest_temp')
+#         pytest_dir = home_dir.mkdir('pytest_temp')
 
-        yield pytest_dir
+#         yield pytest_dir
 
 
-@pytest.fixture()
-def sshdirectory(sshdirectory_pytest, request):
-    """ Creates the py.test fixture to make it usable withing the unit tests.
-    See the Vagrant class for more information.
-    """
+# @pytest.fixture()
+# def sshdirectory(sshdirectory_pytest, request):
+#     """ Creates the py.test fixture to make it usable withing the unit tests.
+#     See the Vagrant class for more information.
+#     """
 
-    assert sshdirectory_pytest.getcwd() == '/home/vagrant/pytest_temp'
+#     assert sshdirectory_pytest.getcwd() == '/home/vagrant/pytest_temp'
 
-    return sshdirectory_pytest.mkdir(request.node.name)
+#     return sshdirectory_pytest.mkdir(request.node.name)
--- test/test_vagrant.py
+++ test/test_vagrant.py
@@ -1,93 +1,100 @@
 import os
 
 
-def test_vagrantfile(vagrant):
-    assert vagrant.vagrant_file() is not None
+def test_vagrant_fixture(vagrant):
+    # We don't do anything just check that the fixture is available
+    pass
+
+def test_vagrant_import():
+    from pytest_vagrant import Vagrant
+
+# def test_vagrantfile(vagrant):
+#     assert vagrant.vagrant_file() is not None
 
 
-def test_version(vagrant):
-    assert vagrant.version() is not None
+# def test_version(vagrant):
+#     assert vagrant.version() is not None
 
 
-def test_status(vagrant):
-    status = vagrant.status
-    assert str(status) == "running"
-    assert status.running == True
-    assert status.not_created == False
-    assert status.poweroff == False
-    assert status.aborted == False
-    assert status.saved == False
+# def test_status(vagrant):
+#     status = vagrant.status
+#     assert str(status) == "running"
+#     assert status.running == True
+#     assert status.not_created == False
+#     assert status.poweroff == False
+#     assert status.aborted == False
+#     assert status.saved == False
 
 
-def test_port(vagrant):
-    assert len(vagrant.port()) != 0
+# def test_port(vagrant):
+#     assert len(vagrant.port()) != 0
 
 
-def test_sshdirectory_path(sshdirectory):
+# def test_sshdirectory_path(sshdirectory):
 
-    # All subdirectories are create in the ~/pytest_temp and the
-    # subdirectories are named after the test-cast e.g. in this
-    # case test_sshdirectory_path
+#     # All subdirectories are create in the ~/pytest_temp and the
+#     # subdirectories are named after the test-cast e.g. in this
+#     # case test_sshdirectory_path
 
-    assert sshdirectory.getcwd() == '/home/vagrant/pytest_temp/test_sshdirectory_path'
-
-
-def test_sshdirectory_put_file(testdirectory, sshdirectory):
-
-    file_path = testdirectory.write_text(
-        "test.txt", data=u"hello", encoding="utf-8")
-
-    sshdirectory.put_file(local_file=file_path)
-    assert sshdirectory.contains_file("test.txt")
-
-    sshdirectory.put_file(local_file=file_path, rename_as="ok.txt")
-    assert sshdirectory.contains_file("ok.txt")
+#     assert sshdirectory.getcwd() == '/home/vagrant/pytest_temp/test_sshdirectory_path'
 
 
-def test_sshdirectory_get_file(testdirectory, sshdirectory):
-    test_dir = sshdirectory.mkdir('testdir')
-    test_dir.run('touch hello_world.txt')
+# def test_sshdirectory_put_file(testdirectory, sshdirectory):
 
-    assert test_dir.contains_file("hello_world.txt")
+#     file_path = testdirectory.write_text(
+#         "test.txt", data=u"hello", encoding="utf-8")
 
-    test_dir.get_file(remote_file="hello_world.txt",
-                      local_directory=testdirectory.path())
+#     sshdirectory.put_file(local_file=file_path)
+#     assert sshdirectory.contains_file("test.txt")
 
-    testdirectory.contains_file("hello_world.txt")
-
-
-def test_sshdirectory_run(sshdirectory):
-    test_dir = sshdirectory.mkdir('testdir')
-    test_dir.run('touch hello_world.txt')
-
-    assert test_dir.contains_file("hello_world.txt")
-
-    res = test_dir.run('ls -la')
-    res.match(stdout="*hello_world.txt*")
+#     sshdirectory.put_file(local_file=file_path, rename_as="ok.txt")
+#     assert sshdirectory.contains_file("ok.txt")
 
 
-def test_sshdirectory_from_path(sshdirectory):
-    tmp_dir = sshdirectory.from_path('/tmp')
-    assert tmp_dir.getcwd() == '/tmp'
+# def test_sshdirectory_get_file(testdirectory, sshdirectory):
+#     test_dir = sshdirectory.mkdir('testdir')
+#     test_dir.run('touch hello_world.txt')
+
+#     assert test_dir.contains_file("hello_world.txt")
+
+#     test_dir.get_file(remote_file="hello_world.txt",
+#                       local_directory=testdirectory.path())
+
+#     testdirectory.contains_file("hello_world.txt")
 
 
-def test_sshdirectory_mkdir_rmdir(sshdirectory):
-    test_dir = sshdirectory.mkdir('testdir')
-    test_dir.run('touch hello')
+# def test_sshdirectory_run(sshdirectory):
+#     test_dir = sshdirectory.mkdir('testdir')
+#     test_dir.run('touch hello_world.txt')
 
-    assert sshdirectory.contains_dir('testdir')
+#     assert test_dir.contains_file("hello_world.txt")
 
-    sshdirectory.rmdir('testdir')
+#     res = test_dir.run('ls -la')
+#     res.match(stdout="*hello_world.txt*")
 
-    assert not sshdirectory.contains_dir('testdir')
+
+# def test_sshdirectory_from_path(sshdirectory):
+#     tmp_dir = sshdirectory.from_path('/tmp')
+#     assert tmp_dir.getcwd() == '/tmp'
 
 
-def test_sshdirectory_rmfile(sshdirectory):
-    test_dir = sshdirectory.mkdir('testdir')
-    test_dir.run('touch hello_world.txt')
+# def test_sshdirectory_mkdir_rmdir(sshdirectory):
+#     test_dir = sshdirectory.mkdir('testdir')
+#     test_dir.run('touch hello')
 
-    assert test_dir.contains_file("hello_world.txt")
+#     assert sshdirectory.contains_dir('testdir')
 
-    test_dir.rmfile("hello_world.txt")
+#     sshdirectory.rmdir('testdir')
 
-    assert not test_dir.contains_file("hello_world.txt")
+#     assert not sshdirectory.contains_dir('testdir')
+
+
+# def test_sshdirectory_rmfile(sshdirectory):
+#     test_dir = sshdirectory.mkdir('testdir')
+#     test_dir.run('touch hello_world.txt')
+
+#     assert test_dir.contains_file("hello_world.txt")
+
+#     test_dir.rmfile("hello_world.txt")
+
+#     assert not test_dir.contains_file("hello_world.txt")
