d7b6e15c716c63db601496ffc78e76ebfd0bf50f
Jeppe Pihl <jpihl08@gmail.com>
2021-09-07 11:05:14 +0200

    fix black

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from setuptools import setup, find_packages
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(cwd, 'README.rst'), encoding='utf-8') as fd:
+with io.open(os.path.join(cwd, "README.rst"), encoding="utf-8") as fd:
     long_description = fd.read()
 
-with io.open(os.path.join(cwd, 'wscript'), encoding='utf-8') as fd:
+with io.open(os.path.join(cwd, "wscript"), encoding="utf-8") as fd:
 
     VERSION = None
 
@@ -27,7 +27,9 @@ with io.open(os.path.join(cwd, 'wscript'), encoding='utf-8') as fd:
          \d+\.\d+\.\d+  #    Match digit(s).digit(s).digit(s) e.g. 10.2.3
     )                   # End of group
     '                   # Match '
-    """, re.VERBOSE)
+    """,
+        re.VERBOSE,
+    )
 
     for line in fd:
 
@@ -40,37 +42,36 @@ with io.open(os.path.join(cwd, 'wscript'), encoding='utf-8') as fd:
         break
 
     else:
-        sys.exit('No VERSION variable defined in wscript - aborting!')
+        sys.exit("No VERSION variable defined in wscript - aborting!")
 
 setup(
-    name='pytest-vagrant',
+    name="pytest-vagrant",
     version=VERSION,
-    description=('A py.test plugin providing access to vagrant.'),
+    description=("A py.test plugin providing access to vagrant."),
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/steinwurf/pytest-vagrant',
-    author='Steinwurf ApS',
-    author_email='contact@steinwurf.com',
+    long_description_content_type="text/x-rst",
+    url="https://github.com/steinwurf/pytest-vagrant",
+    author="Steinwurf ApS",
+    author_email="contact@steinwurf.com",
     license='BSD 3-clause "New" or "Revised" License',
     classifiers=[
-        'Framework :: Pytest',
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Plugins',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Testing',
+        "Framework :: Pytest",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Testing",
     ],
-    keywords=('pytest py.test '
-              'testing unit tests plugin vagrant'),
-    packages=find_packages(where='src', exclude=['test']),
+    keywords=("pytest py.test " "testing unit tests plugin vagrant"),
+    packages=find_packages(where="src", exclude=["test"]),
     package_dir={"": "src"},
-    install_requires=['pytest', 'paramiko', 'python-slugify'],
+    install_requires=["pytest", "paramiko", "python-slugify"],
     entry_points={
-        'pytest11': ['vagrant = pytest_vagrant.fixtures'],
+        "pytest11": ["vagrant = pytest_vagrant.fixtures"],
     },
 )
